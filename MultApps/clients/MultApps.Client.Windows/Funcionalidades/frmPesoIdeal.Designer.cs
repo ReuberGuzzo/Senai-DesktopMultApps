ebd09a0a5504bf52be70ac53e89f9c9b983e0070
Reuber Guzzo <reuber.rg@gmail.com>
2022-09-05 21:53:22 -0300

    Mudança de layout do Program.cs

--- MultApps/clients/MultApps.Client.Windows/Funcionalidades/frmPesoIdeal.Designer.cs
+++ MultApps/clients/MultApps.Client.Windows/Funcionalidades/frmPesoIdeal.Designer.cs
@@ -34,7 +34,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 450);
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(553, 284);
             this.Name = "frmPesoIdeal";
             this.Text = "frmPesoIdeal";
             this.ResumeLayout(false);
